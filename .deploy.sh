692c5035a9a0d48562b3b6fa7ddaf93a6f36ce3e
Kam <kameronkales@gmail.com>
2019-02-17 16:46:04 -0500

    initial commit to store scripts

---
+++ .deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function deploy(){
+  git add .
+
+  echo 'Enter the commit message:'
+  read commitMessage
+
+  git commit -m "$commitMessage"
+
+  echo 'Enter the name of the branch:'
+  read branch
+
+  git push origin $branch
+
+}
+
+function webme() {
+  echo 'what port?'
+  read port
+
+  ~/ngrok http $port
+}
+
+function buildweb(){
+  bundle exec jekyll serve
+}
+
+function website(){
+
+  git add .
+
+  git commit -m "updating website"
+
+  git push origin master
+
+  jgd -n
+}
